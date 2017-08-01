e563fb405174ded0dc341eaca21ab5b5dcf97d16
Edgars Mjasnikovs <EMJA@JWinc.jeld-wen.com>
2017-08-01 07:48:41 +0300

    readme test aded

---
+++ test/readme.js
@@ -0,0 +1,35 @@
+const test = require('tape')
+const schema = require('../index.js')
+
+const schemaObject = {
+	id: schema.number,
+	name: schema.string,
+	age: schema.any([null, schema.naturalNumber]),
+	friendList: [
+		{
+			id: schema.number,
+			name: schema.string
+		}
+	]
+}
+
+const object = {
+	id: 1,
+	name: 'Edgars',
+	age: null,
+	friendList: [
+		{
+			id: 2,
+			name: 'Joe'
+		},
+		{
+			id: 3,
+			name: 'Kate'
+		}
+	]
+}
+
+test('Readme test', t => {
+	schema.test(t, schemaObject, object)
+	t.end()
+})
